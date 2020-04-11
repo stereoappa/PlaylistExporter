26c421128182393a9a2343b48874bc2b571adcd9
Anton Temnov <stereoappa@gmail.com>
2020-04-11 21:59:52 +0400

    Add project files.

---
+++ PlaylistExporter/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PlaylistExporter.Models;
+
+namespace PlaylistExporter.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+      
+       
+    }
+}
---
+++ PlaylistExporter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PlaylistExporter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
