efb2529faa99d6786ce12929357c5d57e6013077
Anton Temnov <stereoappa@gmail.com>
2020-04-11 22:07:34 +0400

    add route

--- PlaylistExporter/Startup.cs
+++ PlaylistExporter/Startup.cs
@@ -50,7 +50,7 @@ namespace PlaylistExporter
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Account}/{action=Login}/{id?}");
             });
         }
     }
