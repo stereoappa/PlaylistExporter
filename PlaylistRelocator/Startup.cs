6148d542d29d39bccb1bcb6aa5eb3d686c7fa06f
Anton Temnov <stereoappa@gmail.com>
2020-05-11 00:55:26 +0400

    fix: Rename project

--- PlaylistRelocator/Startup.cs
+++ PlaylistRelocator/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace PlaylistExporter
+namespace PlaylistRelocator
 {
     public class Startup
     {
