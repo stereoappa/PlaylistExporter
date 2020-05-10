3419adbdfcfad709324a4c7960a176dbedb1d3cc
Anton Temnov <stereoappa@gmail.com>
2020-05-11 01:03:57 +0400

    fix: rename progect done

--- PlaylistRelocator/Program.cs
+++ PlaylistRelocator/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace PlaylistExporter
+namespace PlaylistRelocator
 {
     public class Program
     {
