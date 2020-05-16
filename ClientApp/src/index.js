8e1ecb30b3afab318434a7626a76d921b1699353
Anton Temnov <stereoappa@gmail.com>
2020-05-16 21:51:29 +0400

    workflow added

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,1 +1,1 @@
-const test = '123'
+import './scss/index.scss'
--- ClientApp/webpack.config.js
+++ ClientApp/webpack.config.js
@@ -1,12 +1,76 @@
 const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const isProd = process.env.NODE_ENV === 'production'
+const isDev = !isProd
+
+const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
+
+const jsLoaders = () => {
+  const loaders = [{
+    loader: 'babel-loader',
+    options: {
+      presets: ['@babel/preset-env']
+    }
+  }]
+
+  if (isDev) {
+    loaders.push('eslint-loader')
+  }
+  return loaders
+}
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    entry: './index.js',
-    output: {
-        filename: 'bundle.[hash].js',
-        path: path.resolve(__dirname, 'dist')
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: ['@babel/polyfill', './index.js'],
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  devtool: isDev ? 'source-map' : false,
+  devServer: {
+    port: 3000,
+    hot: isDev
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd
+      }
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    })
+  ],
+  module: {
+    rules: [{
+      test: /\.s[ac]ss$/i,
+      use: [{
+        loader: MiniCssExtractPlugin.loader,
+        options: {
+          hmr: isDev,
+          reloadAll: true
+        }
+      },
+      'css-loader',
+      'sass-loader',
+      ]
     },
-    mode: 'development'
-
+    {
+      // babel and eslint
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: jsLoaders()
+    }
+    ]
+  }
 }
